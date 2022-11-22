985ffca13339f3ea3f3d5fe3aa2da973d3588aa7
David Palumbo <82134315+david-tandem@users.noreply.github.com>
2022-11-22 12:55:00 -0500

    Add credentials to install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+
 cd /workspaces
-git clone https://github.com/david-tandem/personal-sdk.git
+
+if [ "${CODESPACES}" = "true" ]; then
+    # Remove the default credential helper
+    sudo sed -i -E 's/helper =.*//' /etc/gitconfig
+
+    # Add one that just uses secrets available in the Codespace
+    git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
+fi
+
+git clone https://github.com/spaceball-one/spaceballs-the-sdk.git
