2a71069e5f11466a44ee6cf7eca833764f7990f6
David Palumbo <82134315+david-tandem@users.noreply.github.com>
2022-11-22 13:51:22 -0500

    ...

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ "${CODESPACES}" = "true" ]; then
     sudo sed -i -E 's/helper =.*//' /etc/gitconfig
 
     # Add one that just uses secrets available in the Codespace
-    git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
+    git config --global credential.helper echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}";
 fi
 
 git clone https://github.com/david-tandem/personal-sdk.git
